c5cba65e5b6e465bd8a4c7334580ac5f92981a8a
oybek993 <melikulov990@gmail.com>
2020-08-09 15:53:03 +0500

    Updates

---
+++ precache-manifest.056699cab0e6597c93f505d58a693545.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c2808bd05b4d335ec77bac82ca38e8d",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "12f3be3e32f61400315c",
+    "url": "/react-todo/static/css/main.5d3b1af2.chunk.css"
+  },
+  {
+    "revision": "cbc499477bc07bd38976",
+    "url": "/react-todo/static/js/2.6ee71531.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todo/static/js/2.6ee71531.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12f3be3e32f61400315c",
+    "url": "/react-todo/static/js/main.5f98ffd4.chunk.js"
+  },
+  {
+    "revision": "8b60092a6ae08cc228b0",
+    "url": "/react-todo/static/js/runtime-main.40cbbb06.js"
+  }
+]);
